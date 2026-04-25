9b3c8c513b405671eb4450d2b823cdc121d4388d
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-04-25 23:21:58 +0600

    added dyanmic date in header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import logo from "@/assets/logo.png"
+// import logo from "@/assets/logo.png"
 import Image from 'next/image'
+import { format } from 'date-fns';
+
 
 const Header = () => {
   return (
-    <div className='text-center py-8 space-y-4'>
-      <Image src={logo} width={300} height={300} alt='logo' className='mx-auto'></Image>
-      <p>Journalism</p>
-      <p>date</p>
-      {/* formate the data by using datefns package */}
+    <div className="text-center py-8 space-y-4">
+      <Image
+        src={''}
+        width={300}
+        height={300}
+        alt="logo"
+        className="mx-auto"
+      ></Image>
+      <p className="text-xl font-semibold">Journalism</p>
+      <p className="text-xl font-semibold">
+        {' '}
+        {format(new Date(), "EEEE, MMMM d, yyyy")} {' '}
+      </p>
     </div>
-  )
+  );
 }
 
 export default Header
